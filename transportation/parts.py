276fc120079e787f8bb1d4436e22fa6899444fb1
William Couturiaux <37006112+wcouturiaux@users.noreply.github.com>
2022-12-26 19:33:29 -0500

    Add batteries

--- transportation/parts.py
+++ transportation/parts.py
@@ -39,3 +39,23 @@ class SternmanEngine(IEngine):
 
     def needs_service(self):
         return self.warning_light_on
+
+
+class SpindlerBattery(IBattery):
+
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        return self.current_date.year - self.last_service_date.year >= 2
+
+
+class NubbinBattery(IBattery):
+
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        return self.current_date.year - self.last_service_date.year >= 4
