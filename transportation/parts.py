b811445580ae68dafe531ae5636cea80c77eba18
William Couturiaux <37006112+wcouturiaux@users.noreply.github.com>
2022-12-26 19:25:25 -0500

    Add engines

--- transportation/parts.py
+++ transportation/parts.py
@@ -9,3 +9,33 @@ class IBattery(ABC):
 class IEngine(ABC):
     def needs_service(self):
         pass
+
+
+# ENGINES
+class CapuletEngine(IEngine):
+
+    def __init__(self, current_mileage, last_service_mileage):
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage > 30000
+
+
+class WilloughbyEngine(IEngine):
+
+    def __init__(self, current_mileage, last_service_mileage):
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage > 60000
+
+
+class SternmanEngine(IEngine):
+
+    def __init__(self, warning_light_on):
+        self.warning_light_on = warning_light_on
+
+    def needs_service(self):
+        return self.warning_light_on
--- transportation/vehicles.py
+++ transportation/vehicles.py
@@ -1,1 +1,18 @@
-from abc import abc
+from abc import ABC, abstractmethod
+
+
+class IServiceable(ABC):
+
+    @abstractmethod
+    def needs_service(self):
+        pass
+
+
+class Car(IServiceable):
+
+    def __init__(self, last_service_date, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        pass
