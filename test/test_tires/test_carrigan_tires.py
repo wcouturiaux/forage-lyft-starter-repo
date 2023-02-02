e29f1e4de8a5034cf85ad79b770f70a8c12218d2
William Couturiaux <37006112+wcouturiaux@users.noreply.github.com>
2023-02-01 19:12:45 -0500

    Add Carrigan Tire tests

--- test/test_tires/test_carrigan_tires.py
+++ test/test_tires/test_carrigan_tires.py
@@ -1,8 +1,15 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from transportation.tires import CarriganTires
 
-if __name__ == '__main__':
-    unittest.main()
+
+class TestCarriganTires(unittest.TestCase):
+    def test_needs_service_true(self):
+        tire_wear = [0.1, 0.5, 0.7, 0.9]
+        tires = CarriganTires(tire_wear)
+        self.assertTrue(tires.needs_service())
+
+    def test_needs_service_false(self):
+        tire_wear = [0.1, 0.5, 0.5, 0.8]
+        tires = CarriganTires(tire_wear)
+        self.assertFalse(tires.needs_service())
