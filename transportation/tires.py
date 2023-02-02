6ba7f4e251f65c9c26b6d95b4f6cf5172c8a8946
William Couturiaux <37006112+wcouturiaux@users.noreply.github.com>
2023-02-01 19:22:36 -0500

    Implement Carrigan Tire Class

--- transportation/tires.py
+++ transportation/tires.py
@@ -12,3 +12,12 @@ class OctoprimeTires(ITires):
 
     def needs_service(self):
         return sum(self.tire_wear) >= 3.0
+
+
+class CarriganTires(ITires):
+
+    def __init__(self, tire_wear: [4]):
+        self.tire_wear = tire_wear
+
+    def needs_service(self):
+        return any(ele >= 0.9 for ele in self.tire_wear)
